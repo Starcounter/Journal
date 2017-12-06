5abf5ca1f16a8180d43239de9f9fe2ac031cc729
Omar Alshaker <omar@omaralshaker.com>
2017-12-06 15:36:15 +0100

    Enchancements
    1. Remove grouping by org
    2. Wider container
    3. Make dates dd-mm-yyyy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('static'));
 
-let currentData = {};
+let currentData = [];
 
 downloadData().then(data => (currentData = data));
 
--- static/app.js
+++ static/app.js
@@ -1,75 +1,80 @@
 const converter = new showdown.Converter();
 function convertMarkdown(md) {
-    return converter.makeHtml(md);
+  return converter.makeHtml(md);
 }
 
 function checkAndCache(item) {
-    const isRead = localStorage.getItem(item);
-    if (isRead) {
-        return true;
-    }
+  const isRead = localStorage.getItem(item);
+  if (isRead) {
+    return true;
+  }
 }
 
-function itemHasUnreads(item) {
-    for (const repo of item.repos) {
-        for (const release of repo.releases) {
-            const hash = `${item.name}-${repo.repoName}-${release.tag_name}`;
-            release.hash = hash;
-            if (!checkAndCache(hash)) { // unread item
-                return true;
-            }
-        }
+function itemHasUnreads(repo) {
+  for (const release of repo.releases) {
+    const hash = `${repo.repoName}#${release.tag_name}`;
+    release.hash = hash;
+    if (!checkAndCache(hash)) {
+      // unread item
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
-fetch('/data.json').then(res => res.json()).then(data => {
+fetch('/data.json')
+  .then(res => res.json())
+  .then(data => {
     let items = data.results;
+    // each item is an org
+
+    // flatten orgs into repos
+    let repos = [];
     for (const item of items) {
-        item.unreadsCount = 0;
-        for (const repo of item.repos) {
-            for (const release of repo.releases) {
-                const hash = `${item.name}-${repo.repoName}-${release.tag_name}`;
-                release.hash = hash;
-                release.read = checkAndCache(hash);
-                release.body = release.body.trim() || "No notes provided";
-
-                if (!release.read) {
-                    item.unreadsCount++;
-                }
-            }
+      item.unreadsCount = 0;
+      for (const repo of item.repos) {
+        for (const release of repo.releases) {
+          const hash = `${repo.repoName}#${release.tag_name}`;
+          release.hash = hash;
+          release.read = checkAndCache(hash);
+          release.body = release.body.trim() || 'No notes provided';
+          release.unixTimeStamp = new Date(release.published_at) / 1000;
         }
-
+        // sort by release dates
+        repo.releases = repo.releases.sort(
+          (a, b) => b.unixTimeStamp - a.unixTimeStamp
+        );
+        repo.lastReleaseDate = repo.releases[0].unixTimeStamp;
+        repos.push(repo);
+      }
     }
-    items = items.sort((a, b) => b.unreadsCount - a.unreadsCount);
-    new Vue({
-        el: '#app',
-        data: {
-            items,
-            updatedOn: data.date,
-            appLoaded: true
-        },
-        methods: {
-            formatDate(str) {
-                const date = new Date(str);
-                return date.toLocaleDateString();
-            },
-            setAsRead(release) {
-                release.read = true;
-                localStorage.setItem(release.hash, true);
-            },
-            markAllAsRead() {
-                for (const item of this.items) {
-                    for (const repo of item.repos) {
-                        for (const release of repo.releases) {
-                            this.setAsRead(release);
-                        }
-                    }
+    // sort them by date
+    repos = repos.sort((a, b) => b.lastReleaseDate - a.lastReleaseDate);
 
-                    item.unreadsCount = 0;
-                }
-                this.items = this.items;
+    new Vue({
+      el: '#app',
+      data: {
+        repos,
+        updatedOn: data.date,
+        appLoaded: true
+      },
+      methods: {
+        formatDate(str) {
+          const date = new Date(str);
+          return date.toLocaleDateString('en-GB');
+        },
+        setAsRead(release) {
+          release.read = true;
+          localStorage.setItem(release.hash, true);
+        },
+        markAllAsRead() {
+          for (const repo of this.repos) {
+            for (const release of repo.releases) {
+              this.setAsRead(release);
             }
+          }
+          this.repos = this.repos;
         }
-    })
-});
+      }
+    });
+  });
